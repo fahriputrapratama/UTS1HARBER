50a4c904408e011c69f1ddba4ced7b02f01ad6fc
fahriputrapratama <fahripratama14205@gmail.com>
2023-11-13 09:10:58 +0700

    UTS HARBER

--- src/SoalNo3.java
+++ src/SoalNo3.java
@@ -1,2 +1,15 @@
-package PACKAGE_NAME;public class SoalNo3 {
+import java.util.Scanner;
+
+public class SoalNo3 {
+    public static void main(String[] args) {
+        System.out.println(" Masukan jenis suhu yang akan dikonversi ( Fahrenheit / Reamur)");
+        Scanner suhu=new Scanner(System.in);
+        int number = suhu.nextInt();
+
+        double Reamur;
+        double Fahrenheit;
+        int Fahrenheit=number;
+
+
+    }
 }
