66b471f68ef5bf8d39129d043e16318094fbb91c
fahriputrapratama <fahripratama14205@gmail.com>
2023-11-13 11:17:47 +0700

    UTS HARBER

--- src/SoalNo3.java
+++ src/SoalNo3.java
@@ -1,15 +1,19 @@
 import java.util.Scanner;
-
 public class SoalNo3 {
-    public static void main(String[] args) {
-        System.out.println(" Masukan jenis suhu yang akan dikonversi ( Fahrenheit / Reamur)");
-        Scanner suhu=new Scanner(System.in);
-        int number = suhu.nextInt();
+    public static void main(String args[]){
+        Scanner input = new Scanner(System.in);
+        System.out.println("Konversi SuhuCelcius ");
 
-        double Reamur;
-        double Fahrenheit;
-        int Fahrenheit=number;
+        double celc, fahren, reamur;
 
+        System.out.print("Input Suhu Celsius: ");
+        celc = input.nextDouble();
+
+        fahren = (9.0/5.0 * celc) + 32;
+        reamur = celc * (4.0/5.0) ;
+
+        System.out.println(celc+" Celcius = "+fahren+" Fahrenheit");
+        System.out.println(celc+" Celcius = "+reamur+" Reamur");
 
     }
 }
